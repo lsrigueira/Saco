d14d179efc9171efc7bfb2adcb396dbaae04a8fd
PrivatedGuest <34251683+PrivatedGuest@users.noreply.github.com>
2019-07-31 12:54:59 +0200

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,10 +1,8 @@
 NSAMPLES=10#The number of samples we receive from each axis of each json
 PATH="JsonsCollidos/"#Where are all the jsons?
 NJSONS=9#remember the name needs to be the next in the sequence(30,31,34,35,38,39,42....)
-#GOLPES=["Gancho","Recto","Crochet","Mixto","Patada circular","Patada giratoria","Patada voladora","Patada gancho"]#The name needs to have 3 characters minimum
-#DIRECCIONS=["Ascendente","Frontal","Lateral Dereito","Lateral Esquerdo","Descendente"]
 GOLPES=["Crochet","Golpe","Patada"]
-DIRECCIONS=["Derecha","Izquierda","Frontal"]
+DIRECCIONS=["Dereita","Esquerda","Frontal"]
 RANDOM_STATE=0#this is for the algorith
 MAXITER=1000000
 NSA_PW=10  #Numero de mostras coas que facemos a media de potencia
